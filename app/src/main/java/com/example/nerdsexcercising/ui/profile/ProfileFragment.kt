c6f86665dac9b7b666e5ae859d199acd3b9513d7
shaygeko <shramko.georgiy@gmail.com>
2022-02-20 04:30:48 -0800

    :sparkles: logout

--- app/src/main/java/com/example/nerdsexcercising/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/nerdsexcercising/ui/profile/ProfileFragment.kt
@@ -1,11 +1,21 @@
 package com.example.nerdsexcercising.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.nerdsexcercising.R
+import com.example.nerdsexcercising.ui.login.LoginActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.material.button.MaterialButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +31,7 @@ class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var auth : FirebaseAuth;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +39,8 @@ class ProfileFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+        auth = Firebase.auth
     }
 
     override fun onCreateView(
@@ -38,6 +51,32 @@ class ProfileFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val logoutBtn = view.findViewById<MaterialButton>(R.id.logout_btn);
+
+        logoutBtn.setOnClickListener {
+            logout(view)
+        }
+    }
+    fun logout(view: View){
+        Log.d("ProfileFragment", "Logout");
+
+
+        auth.signOut();
+        GoogleSignIn.getClient(
+            requireContext(),
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build()
+        ).signOut().addOnCompleteListener {
+            Log.d("LOGOUT", "logged out successfully")
+            val logoutIntent = Intent(view.context, LoginActivity::class.java);
+            logoutIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(logoutIntent)
+        }
+
+
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
